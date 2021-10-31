eca40251b5e5e89f42df12d4b79f43282b1a2b07
Shimanogov <37351887+Shimanogov@users.noreply.github.com>
2021-10-31 23:22:42 +0300

    added redis support

--- web/main.py
+++ web/main.py
@@ -1,15 +1,13 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel, Field
-
-
-class InputURL(BaseModel):
-    url: str = Field(..., title='Text', description='Input URL to be shorten', max_length=300)
-
+import redis
+import coolname
 
 app = FastAPI(title='Make your URL shorts')
 templates = Jinja2Templates(directory="pages")
+
+main_db = redis.Redis(host='redis')
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,6 +15,7 @@ async def read_item(request: Request):
     return templates.TemplateResponse("landing.html", {"request": request})
 
 
-@app.post("/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("landing.html", {"request": request})
+@app.post("/")
+async def login(input_url: str = Form(...)):
+    main_db.set(input_url, coolname.generate_slug())
+    return {"input_url": input_url}
