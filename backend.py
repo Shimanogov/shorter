bed64a17560b3b839e3a80f655a98804990985d7
Shimanogov <37351887+Shimanogov@users.noreply.github.com>
2021-11-01 17:16:15 +0300

    Created bot links

--- backend.py
+++ backend.py
@@ -1,3 +1,5 @@
+from hashlib import shake_128
+
 import coolname
 import redis
 from fastapi import FastAPI, Request, Form
@@ -7,7 +9,8 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI(title='Make your URL shorts')
 templates = Jinja2Templates(directory=".")
 
-main_db = redis.StrictRedis(host='redis', decode_responses=True)
+main_db = redis.StrictRedis(host='redis', decode_responses=True, db=0)
+redirection_db = redis.StrictRedis(host='redis', decode_responses=True, db=1)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -17,13 +20,26 @@ async def read_item(request: Request):
 
 @app.post("/", response_class=HTMLResponse)
 async def login(request: Request, input_url: str = Form(...)):
-    short = coolname.generate_slug(3)
-    main_db.set(short, input_url)
-    return templates.TemplateResponse("web-page.html", {"request": request, "short": short})
-    # TODO: pass name from docker compose
+    values = main_db.hgetall(input_url)
+    if values == {}:
+        values = {
+            'human': coolname.generate_slug(3),
+            'bot': shake_128(input_url.encode('utf-8')).hexdigest(3),
+        }
+        main_db.hmset(input_url, values)
+        redirection_db.mset({values['bot']: input_url, values['human']: input_url})
+    return templates.TemplateResponse("web-page.html", {'request': request,
+                                                        'human': values['human'],
+                                                        'bot': values['bot'], })
+    # TODO: pass name for webpage from docker compose
+    # TODO: expiration time
+    # TODO: check for collisions
 
 
 @app.get("/{short}", response_class=RedirectResponse)
 async def read_item(short: str):
-    long = main_db.get(short)
+    long = redirection_db.get(short)
     return long
+    # TODO: create oops fallback
+    # TODO: make also work as shorter
+    # TODO: reset expiration
