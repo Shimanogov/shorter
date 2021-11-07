e0b04e307296697bdf50913c7099ef5d20201109
Shimanogov <37351887+Shimanogov@users.noreply.github.com>
2021-11-07 17:16:26 +0300

    optimized backend

--- backend.py
+++ backend.py
@@ -6,6 +6,13 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
+# TODO: make separate config file
+expiration_time = 60 * 60 * 24 * 7 * 2
+bot_length = 3
+human_length = 2
+collision_fix_times = 10
+domain = 'shorter.fit'
+
 app = FastAPI(title='Make your URL shorts')
 templates = Jinja2Templates(directory=".")
 
@@ -15,7 +22,8 @@ redirection_db = redis.StrictRedis(host='redis', decode_responses=True, db=1)
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("web-page.html", {"request": request})
+    return templates.TemplateResponse("web-page.html", {"request": request,
+                                                        'domain': domain})
 
 
 @app.post("/", response_class=HTMLResponse)
@@ -24,26 +32,33 @@ async def login(request: Request, input_url: str = Form(...)):
     if values == {}:  # check if url already shorted
 
         values = {  # generate new values
-            'human': coolname.generate_slug(2),
-            'bot': shake_128(input_url.encode('utf-8')).hexdigest(3),
+            'human': coolname.generate_slug(human_length),
+            'bot': shake_128(input_url.encode('utf-8')).hexdigest(bot_length),
         }
 
-        while redirection_db.get(values['bot']):  # check for collisions
-            values['bot'] = shake_128((input_url + values['bot']).encode('utf-8')).hexdigest(3)
+        for _ in range(collision_fix_times):
+            if redirection_db.get(values['bot']):  # check for collisions
+                values['bot'] = shake_128((input_url + values['bot']).encode('utf-8')).hexdigest(bot_length)
+            else:
+                break
 
-        while redirection_db.get(values['human']):  # check for collisions
-            values['human'] = coolname.generate_slug(2)
+        for _ in range(collision_fix_times):
+            if redirection_db.get(values['human']):  # check for collisions
+                values['human'] = coolname.generate_slug(human_length)
+            else:
+                break
 
         main_db.hmset(input_url, values, )  # add new link
         redirection_db.mset({values['bot']: input_url, values['human']: input_url})  # make redirections
 
-    main_db.expire(input_url, 60 * 60 * 24 * 7 * 2)
-    redirection_db.expire(values['bot'], 60 * 60 * 24 * 7 * 2)
-    redirection_db.expire(values['human'], 60 * 60 * 24 * 7 * 2)
+    main_db.expire(input_url, expiration_time)
+    redirection_db.expire(values['bot'], expiration_time)
+    redirection_db.expire(values['human'], expiration_time)
 
     return templates.TemplateResponse("web-page.html", {'request': request,
                                                         'human': values['human'],
-                                                        'bot': values['bot'], })
+                                                        'bot': values['bot'],
+                                                        'domain': domain})
 
 
 @app.get('/favicon.ico', response_class=FileResponse)
@@ -58,10 +73,11 @@ async def read_item(request: Request, short: str):
     if long:
         # now lets set up expirations:
         values = main_db.hgetall(long)
-        main_db.expire(long, 60 * 60 * 24 * 7 * 2)
-        redirection_db.expire(values['bot'], 60 * 60 * 24 * 7 * 2)
-        redirection_db.expire(values['human'], 60 * 60 * 24 * 7 * 2)
+        main_db.expire(long, expiration_time)
+        redirection_db.expire(values['bot'], expiration_time)
+        redirection_db.expire(values['human'], expiration_time)
 
         return RedirectResponse(long)
     else:
-        return HTMLResponse(templates.TemplateResponse('oops.html', {'request': request}))
+        return HTMLResponse(templates.TemplateResponse('oops.html', {'request': request,
+                                                                     'domain': domain}))
